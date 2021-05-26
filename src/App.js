522a3ddf94e84f9938b13fef905928ce3e13a43e
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-26 13:29:48 +0100

    creating routes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import './styles/app.scss';
 //Import components
 import Cards from './components/Cards';
 import PokemonCard from './components/PokemonCard';
+//Import react router modules
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Here are some of my favourite pokemons</h1>
-      <Cards />
-      <PokemonCard />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <h1>Here are some of my favourite pokemons</h1>
+          <Switch>
+            <Route path="/" exact component={Cards} />
+            <Route path="/pokemon" component={PokemonCard} />
+          </Switch>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
